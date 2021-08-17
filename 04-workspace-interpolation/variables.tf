1a7d00148f06d2e932318918f8c7cfa45cd99b7b
Julian Edington <julian.edington@outlook.com>
2021-08-17 15:36:43 -0500

    added parts 3 and 4

---
+++ 04-workspace-interpolation/variables.tf
@@ -0,0 +1,24 @@
+variable "instance_type" {
+  type = map
+  
+  default = {
+   default = "t2.nano"
+   stage = "t2.micro"
+   prod = "t2.large"
+
+  }
+}
+variable "region" {
+  default = "us-east-1"
+}
+variable "http_port" {
+    default = 80
+}
+variable "ssh_port" {
+    default = 22
+}
+variable "ami" {
+  default = "ami-0a313d6098716f372"
+}
+
+

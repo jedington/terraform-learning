ff37f93cad37e3283fba98def69e683af6e7a9e4
Julian Edington <julian.edington@outlook.com>
2021-08-17 20:20:58 -0500

    added in

---
+++ 03-provisioner-local-exec/output.tf
@@ -0,0 +1,13 @@
+# use this or self hosted option: https://github.com/mpolden/echoip
+data "http" "my_public_ip" {
+  url = "https://ifconfig.co/json"
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+locals {
+  ifconfig_co_json = jsondecode(data.http.my_public_ip.body)
+}
+output "my_ip_addr" {
+  value = local.ifconfig_co_json.ip
+}
---
+++ 03-provisioner-local-exec/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "http_port" {
+    default = 80
+}
+variable "ssh_port" {
+    default = 22
+}
+variable "ami" {
+  default = "ami-0a313d6098716f372"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+

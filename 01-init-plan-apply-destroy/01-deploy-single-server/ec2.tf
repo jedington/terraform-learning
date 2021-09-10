bed57b1a46d73953cdca685fc282bbdee34024ac
Julian Edington <julian.edington@outlook.com>
2021-09-10 06:10:04 -0500

    removed arguably redundant lesson

--- 01-init-plan-apply-destroy/01-deploy-single-server/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "Hello-World" {
-  ami           = "ami-0a313d6098716f372"
-  instance_type = "t2.micro"
-  key_name      = "terraform"
-  tags = {
-    Name = "Hello-World"
-  }
-}

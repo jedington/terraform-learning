c90b5cd3649dd38eb2d2d67a1de131b8aa6433e5
Julian Edington <julian.edington@outlook.com>
2021-10-02 09:08:23 -0500

    deploy jenkins ci

---
+++ 01-init-plan-apply-destroy/03-deploy-jenkins-ci/security_group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "allow_login" {
+  description = "Allow  inbound traffic"
+  ingress {
+    description = "allow ssh to system"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = ["${local.ifconfig_co_json.ip}/32"]
+    #  # alt option - hard coded local system
+    #- cidr_blocks = ["${var.my_system}"]
+  }
+  ingress {
+    description = "allow proxy to system"
+    from_port   = var.proxy_port
+    to_port     = var.proxy_port
+    protocol    = "tcp"
+    cidr_blocks = ["${local.ifconfig_co_json.ip}/32"]
+    #  # alt option - hard coded local system
+    #- cidr_blocks = ["${var.my_system}"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

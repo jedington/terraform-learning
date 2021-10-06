cbfde61a763a500172f0c264c7da7a1e0e357173
Julian Edington <julian.edington@outlook.com>
2021-10-06 17:23:59 -0500

    fmt/aesthetics, flexible ami, conn moved in prov

--- 01-init-plan-apply-destroy/04-deploy-ansible-cm/resources.tf
+++ 01-init-plan-apply-destroy/04-deploy-ansible-cm/resources.tf
@@ -1,26 +1,29 @@
 ### ANSIBLE ###
 resource "aws_instance" "ansible" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  key_name        = "terraform"
-  security_groups = ["${aws_security_group.webserver_sg.id}"]
+  ami                     = var.ami["us-east-1"]
+  instance_type           = var.instance_type
+  key_name                = "terraform"
+  #- security_groups      = ["${aws_security_group.webserver_sg.id}"]
+  vpc_security_group_ids  = ["${aws_security_group.webserver_sg.id}"]
   tags = {
-    Name = var.project
-    OS = "ubuntu"
+    Name  = var.project
+    OS    = "Ubuntu"
   }
-  connection {
-    type            = "ssh"
-    user            = "ec2-user"
-    private_key     = "${file("terraform.pem")}" # have .pem file ready
-    host            = "${self.public_ip}"
-  }
-  # temp example, thinking of making a more modern ansible deployment later
+  # Temp example, thinking of making a more modern ansible deployment later.
+  # Provisioners are a last resort, mostly why I would like to make my own... 
+  # version of an ansible template, but we'll work with this temp one for now.
   provisioner "remote-exec" {
-    inline          = [
+    inline = [
       "sudo amazon-linux-extras install ansible2 -y",
       "sudo apt install git -y",
       "git clone https://github.com/devops-school/ansible-hello-world-role /tmp/ans_ws",
       "ansible-playbook /tmp/ans_ws/site.yaml"
     ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file("terraform.pem")}" # have .pem file ready
+      host        = "${self.public_ip}"
+    }
   }
 }

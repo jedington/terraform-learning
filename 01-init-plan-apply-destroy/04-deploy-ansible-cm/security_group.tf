e2f29e325c52d163fe03ff57282976766643c18c
Julian Edington <julian.edington@outlook.com>
2021-10-06 17:01:02 -0500

    added proxy per ansible src

--- 01-init-plan-apply-destroy/04-deploy-ansible-cm/security_group.tf
+++ 01-init-plan-apply-destroy/04-deploy-ansible-cm/security_group.tf
@@ -1,6 +1,15 @@
 resource "aws_security_group" "webserver_sg" {
   description = "Allow  inbound traffic"
   ingress {
+    description = "allow proxy to system"
+    from_port   = var.proxy_port
+    to_port     = var.proxy_port
+    protocol    = "tcp"
+    cidr_blocks = ["${local.ifconfig_co_json.ip}/32"]
+    #  # alt option - hard coded local system
+    #- cidr_blocks = ["${var.my_system}"]
+  }
+  ingress {
     description = "allow http to system"
     from_port   = var.http_port
     to_port     = var.http_port
